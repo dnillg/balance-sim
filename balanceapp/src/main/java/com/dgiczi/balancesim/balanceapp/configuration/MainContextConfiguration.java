9c12b8fcdc52c738075f49e7269679483bfebe21
Daniel Giczi <Daniel_Giczi@epam.com>
2017-04-13 19:46:53 +0200

    Initial commit

---
+++ balanceapp/src/main/java/com/dgiczi/balancesim/balanceapp/configuration/MainContextConfiguration.java
@@ -0,0 +1,8 @@
+package com.dgiczi.balancesim.balanceapp.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MainContextConfiguration {
+
+}
